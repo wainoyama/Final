481137408d399b3ee4260092f6543a751edd6f5f
YouBhoy <joseemmanuelsilva06@gmail.com>
2024-11-08 12:58:33 +0800

    Fixed the login redirection

--- login.php
+++ login.php
@@ -1,36 +1,41 @@
-<?php
-$validUsername = 'BSIT Student';
-$validPassword = '1234';
+    <?php
+    $validUsername = 'BSIT Student';
+    $validPassword = '1234';
 
-$message = '';
+    $message = '';
 
-if (isset($_POST['submit'])) { 
-    $username = $_POST['username'] ?? '';
-    $password = $_POST['password'] ?? '';
+    if (isset($_POST['submit'])) { 
+        $username = $_POST['username'] ?? '';
+        $password = $_POST['password'] ?? '';
 
-    if ($username === $validUsername && $password === $validPassword) {
-        $message = 'Welcome, ' . $username . '!'; // or yung command na nag reredirect sa home page
-    } else {
-        $message = 'Invalid username or password.';
+        if ($username === $validUsername && $password === $validPassword) {
+header('Location: index.php'); 
+exit;
+} else {
+            $message = 'Invalid username or password.';
+        }
     }
-}
-?>
+    ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body>
-    <h2>Login</h2>
-    <form method='post'>
-        <p>Username: <input type='text' name='username' required></p>
-        <p>Password: <input type='password' name='password' required></p>
-        <p><input type='submit' name='submit' value='Login'></p> 
-    </form>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <h2>Login</h2>
+        <form method='post'>
+            <p>Username: <input type='text' name='username' required></p>
+            <p>Password: <input type='password' name='password' required></p>
+            <p><input type='submit' name='submit' value='Login'></p> 
+        </form>
 
-    <p>
-        <?php echo $message; ?>
-    </p>
-</body>
-</html>
+        <p>
+            <?php echo $message; ?>
+        </p>
+
+        <p>
+            Dont have an account? <a href="signup.php">Click here to sign up</a>
+        </p>
+    </body>
+    </html>
